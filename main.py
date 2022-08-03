7f5067bce18a04b31ae8466e390c798619cab707
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-08-03 14:36:19 +0500

    host fixed

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ async def get_image(passport: str, image: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
